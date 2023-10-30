0e28fc341ef87c69fcb4b5b8c4ac2888677eb2e3
Felipe <90115621+felipesabbado@users.noreply.github.com>
2023-10-30 17:55:06 +0000

    updates

---
+++ ScreenMatch/src/br/com/alura/screenmatch/excecao/ConversaoDeAnoException.java
@@ -0,0 +1,14 @@
+package br.com.alura.screenmatch.excecao;
+
+public class ConversaoDeAnoException extends RuntimeException {
+    private String mensagem;
+
+    public ConversaoDeAnoException(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.mensagem;
+    }
+}
