3e709a8ea76efb307ae1320eaa505fcd4193b081
Felipe <90115621+felipesabbado@users.noreply.github.com>
2023-12-04 17:47:41 +0000

    Projeto com Spring Framework

---
+++ screenmatch2/src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch2;
+
+import br.com.alura.screenmatch2.models.DadosSerie;
+import br.com.alura.screenmatch2.service.ConsumoApi;
+import br.com.alura.screenmatch2.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Screenmatch2Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Screenmatch2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
