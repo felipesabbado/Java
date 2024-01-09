1f195a3f84a776aadd75e612e6ad9c48cf3e805e
Felipe <90115621+felipesabbado@users.noreply.github.com>
2024-01-09 14:16:48 +0000

    jpa hibernate

---
+++ curso-jpa-hibernate/src/main/java/com/sabbado/loja/testes/CadastroDeProduto.java
@@ -0,0 +1,25 @@
+package com.sabbado.loja.testes;
+
+import com.sabbado.loja.modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+public class CadastroDeProduto {
+
+    public static void main(String[] args) {
+        Produto celular = new Produto();
+        celular.setNome("Xiaomi Redmi");
+        celular.setDescricao("Muito legal");
+        celular.setPreco(new BigDecimal("800"));
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(celular);
+        em.getTransaction().commit();
+        em.close();
+    }
+}
