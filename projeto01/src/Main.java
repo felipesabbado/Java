0cc36eebb5eddb45e6f4486e1a5b494c08fe266b
Felipe <90115621+felipesabbado@users.noreply.github.com>
2024-01-11 18:49:16 +0000

    Collections: Lista de Objetos

--- projeto01/src/Main.java
+++ projeto01/src/Main.java
@@ -1,19 +1,5 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        /*
-        int celsius = 33;
-        Calculos.celsiusParaFahrenheit(celsius);
 
-        int nFibonacci = 15;
-        System.out.println(Calculos.fibonacciRecursivo(nFibonacci));
-        System.out.println(Calculos.fibonacciIterativo(nFibonacci));
-        */
-        String str = "GeeksForGeeks";
-        char[] list = str.toCharArray();
-
-        ArrayList<Character> list1 = new ArrayList<>();
-        System.out.println(list1.size());
     }
 }
